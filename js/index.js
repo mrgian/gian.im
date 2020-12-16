23e1d649baae413c23a9d0c995c931c1da153b15
mrgian <gianmatteo.palmieri@gmail.com>
2020-12-16 16:42:44 +0100

    First commit

---
+++ js/index.js
@@ -0,0 +1,9 @@
+function toggleMobileNavState() {
+    const body = document.querySelector("body");
+    body.classList.toggle("nav--active");
+}
+function initBurger() {
+    const burger = document.querySelector(".burger");
+    burger.addEventListener("click", toggleMobileNavState);
+}
+initBurger();
